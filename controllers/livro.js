1c060114d4fbfe585b7812e5d9a0bdc063bb36f2
Thiago Cordeiro <thiiagoccordeirodantas@gmail.com>
2023-05-30 11:04:59 -0700

    Get de dados em arquivoS

--- controllers/livro.js
+++ controllers/livro.js
@@ -1,7 +1,10 @@
+const fs = require("fs")
+
 function getLivros(req, res) {
 
     try {
-        res.send("Ola thiago")
+       const livros = JSON.parse(fs.readFileSync("livros.json"))
+       res.send(livros)
     } catch(error) {    
         res.status(500)
         res.send(error.message)
