238af8fd215e8b1e539cf41279f69b5914c55137
Thiago Cordeiro <thiiagoccordeirodantas@gmail.com>
2023-05-30 11:13:20 -0700

    Criando services

--- controllers/livro.js
+++ controllers/livro.js
@@ -1,9 +1,10 @@
 const fs = require("fs")
+const { getTodosLivros } = require("../services/livro")
 
 function getLivros(req, res) {
 
     try {
-       const livros = JSON.parse(fs.readFileSync("livros.json"))
+       const livros = getTodosLivros()
        res.send(livros)
     } catch(error) {    
         res.status(500)
---
+++ services/livro.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+function getTodosLivros(){
+   return  JSON.parse(fs.readFileSync("livros.json"))
+}
+
+
+
+
+module.exports = {
+    getTodosLivros
+}
